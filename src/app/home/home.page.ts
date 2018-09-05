5685b8dac2da1e4c18c3dfa0c2f63c2cc1faa9dc
keshavs <keshavs@vmware.com>
2018-09-05 11:22:09 +0530

    adding content

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage implements OnInit{
+  public isLoading: boolean = true;
+  public details: any = [];
+
+  constructor(private http: HttpClient){
+
+  }
+
+  private getDetailsApi(){
+    let url = './assets/json/violation-details.json';
+    return this.http.get(url);
+  }
+
+  private getDetails(){
+    this.isLoading = true;
+    this.getDetailsApi().subscribe((data: any)=>{
+      this.details = (data && data.items) || [];
+      this.isLoading = false;
+    });
+  }
+
+  ngOnInit(){
+    this.getDetails();
+  }
+
+}
--- src/app/rules/rules.page.ts
+++ src/app/rules/rules.page.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-rules',
   templateUrl: 'rules.page.html',
   styleUrls: ['rules.page.scss']
 })
-export class RulesPage {}
+export class RulesPage {
+  public isLoading: boolean = true;
+  public details: any = [];
+
+  constructor(private http: HttpClient){
+
+  }
+
+  private getDetailsApi(){
+    let url = './assets/json/police-rules.json';
+    return this.http.get(url);
+  }
+
+  private getDetails(){
+    this.isLoading = true;
+    this.getDetailsApi().subscribe((data: any)=>{
+      this.details = (data && data.items) || [];
+      this.isLoading = false;
+    });
+  }
+
+  ngOnInit(){
+    this.getDetails();
+  }
+}
